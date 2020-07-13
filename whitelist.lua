be3488b64eee4cd1d6be12e2ea2fe667b7263628
socky-duck <67070729+socky-duck@users.noreply.github.com>
2020-07-12 21:35:21 -0400

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,5 +1,12 @@
 local whitelist = {
 ["mudfot"] = "U2e_JGasndDHfru9"
 };
+local reee = whitelist[game.Players.LocalPlayer.Name]
+local key = reee
+if table.find(whitelist, game.Players.LocalPlayer.Name) and key == _G.Key then
+    print("whitelisted")
+    else
+    print("not whitelisted")
+end
 
 return whitelist
